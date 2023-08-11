147a0e57a114d27b6979943b33988c3047f3b1d6
deep-bansal <deepvinaybansal@yahoo.com>
2023-08-11 10:55:04 +0530

    First 5 exps

---
+++ 2.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main(){
+    while(1);
+
+}
+/*dr-xr-xr-x  9 parallels        parallels                      0 Aug 10 21:10 18490*/
---
+++ 4.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <errno.h>
+
+int main() {
+    int fd;
+
+    fd = open("sample.txt", O_RDWR | O_EXCL);
+
+    if (fd == -1) {
+        if (errno == EEXIST) {
+            printf("File already exists. Opening in read-write mode.\n");
+        } else {
+            perror("Error opening file");
+            return 1;
+        }
+    } else {
+        printf("File opened successfully in read-write mode.\n");
+    }
+
+    
+
+    // Close the file
+    close(fd);
+
+    return 0;
+}
